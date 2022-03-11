757e90081e76a5edbf335e1d5b2dde9ba4610f08
alexmwaura <alexmwaura43@gmail.com>
2022-03-11 20:18:46 +0300

    refactor

--- wagchat/src/ChatSidebar.js
+++ wagchat/src/ChatSidebar.js
@@ -1,10 +1,9 @@
-import Searchbar from './Searchbar';
 
 function ChatSideBar() {
   return (
     <div id='plist' className='people-list'>
       <ul className='list-unstyled chat-list mt-2 mb-0'>
-        <li className='clearfix active'>
+        <li className='clearfix active mb-2 mt-5'>
           <img
             src='https://bootdey.com/img/Content/avatar/avatar7.png'
             alt='avatar'
@@ -12,11 +11,11 @@ function ChatSideBar() {
           <div className='about'>
             <div className='name'>Christian Kelly</div>
             <div className='status'>
-              {' '}
               <i className='fa fa-circle offline'></i> left 10 hours ago{' '}
             </div>
           </div>
         </li>
+        
       </ul>
     </div>
   );
